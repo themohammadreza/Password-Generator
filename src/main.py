088db76d087d41aea1da56f7ddc027d14cba1f23
Mohammadreza Naseri <themohammadreza2006@gmail.com>
2025-08-15 19:40:28 +0330

    refacture Memorable class via different word source

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import random
 import string
-from wordfreq import top_n_list
+import os
 from abc import ABC, abstractmethod
 
 
@@ -26,24 +26,31 @@ class RandomPasswordGenerator(PasswordGenerator):
 
 
 class MemorablePasswordGenerator(PasswordGenerator):
-    def __init__(self,
-                 words_no: int = 6,
-                 seperator: str = '-',
-                 capitalize: bool = False,
+    def __init__(
+        self,
+        words_no: int = 4,
+        separator: str = '-',  # Fixed spelling
+        capitalize: bool = False,
     ):
         self.words_no = words_no
-        self.seperator = seperator
+        self.separator = separator  # Fixed spelling
         self.capitalize = capitalize
+        self.vocabulary = self._load_wordlist()
 
-        self.vocabulary = top_n_list("en", self.words_no)
+    def _load_wordlist(self) -> list[str]:
+        wordlist_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "..", "data", "eff_large_wordlist.txt")
+        )
+        with open(wordlist_path, "r") as f:
+            return [line.split()[1] for line in f]
 
     def generate(self) -> str:
-        password_words = self.seperator.join([random.choice(self.vocabulary) for _ in range(self.words_no)])
-
-        if self.capitalize:
-            password_words = password_words.upper()
-
-        return password_words
+        password_words = self.separator.join(
+            random.choice(self.vocabulary) 
+            for _ in range(self.words_no)
+        )
+        
+        return password_words.upper() if self.capitalize else password_words
 
 
 class PinGenerator(PasswordGenerator):
@@ -54,5 +61,5 @@ class PinGenerator(PasswordGenerator):
         return ''.join([random.choice(string.digits) for _ in range(self.length)])
 
 
-password = MemorablePasswordGenerator(capitalize=True)
+password = MemorablePasswordGenerator()
 print(password.generate())
